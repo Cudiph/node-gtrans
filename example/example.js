ec4204399ecc516ede3727aa60c1426bc4c93e6e
Cudiph <dwiaceromo@gmail.com>
2020-12-27 12:25:16 +0700

    example now can translate more than single word an add parameter options

--- example/example.js
+++ example/example.js
@@ -1,78 +1,86 @@
 const gtrans = require('../dist/index.js');
 
-async function trans(text) {
-  const translated = await gtrans(text, { to: 'id' }).then(res => res.data);
+async function trans(text, options = {}) {
+  if (typeof options !== 'object') throw new TypeError('INVALID_TYPE');
+  const translated = await gtrans(text, options).then(res => res.data);
 
-  console.log(`Word(s) "${translated.sourceText}" has meaning "${translated.translated}"`); 
+  console.log(`Word(s) "${text}" has meaning "${translated.translated}"`);
 
-  if (translated.isCorrected) {
-    console.log(`did you mean "${translated.corrected.replace(/\*/g, '')}"?`);
-  }
+  if (translated.pronunciation) console.log(`${translated.sourceText} is pronounced "${translated.pronunciation}"`);
+
+  if (translated.isCorrected) console.log(`did you mean "${translated.corrected.replace(/\*/g, '')}"?`);
 
   // translations
-  for (const key in translated.translations) {
-    let longest = 0;
-    console.log();
-    // find longest string
-    translated.translations[key].forEach(el => {
-      if (el.word.length > longest) longest = el.word.length + 1;
-    })
+  if (translated.translations) {
+    for (const key in translated.translations) {
+      let longest = 0;
+      console.log();
+      // find longest string
+      translated.translations[key].forEach(el => {
+        if (el.word.length > longest) longest = el.word.length + 1;
+      })
 
-    // because it's left & right so divide by 2 and 31 is from 62 / 2 and - 1 is from spacebar from either side
-    console.log(`${'='.repeat((longest / 2) + 31 - 1)} ${key} ${'='.repeat((longest / 2) + 31 - 1)}`);
+      // because it's left & right so divide by 2 and 31 is from 62 / 2 and - 1 is from spacebar from either side
+      console.log(`${'='.repeat((longest / 2) + 31 - 1)} ${key} ${'='.repeat((longest / 2) + 31 - 1)}`);
 
-    // 62 is just brute force of the longest line to make it neat lol
-    console.log('-'.repeat(longest + 62 + key.length))
-    console.log(`word ${' '.repeat(longest - 4)}| translations ${' '.repeat(50 - 12)}| frequency`);
-    console.log('-'.repeat(longest + 62 + key.length))
+      // 62 is just brute force of the longest line to make it neat lol
+      console.log('-'.repeat(longest + 62 + key.length))
+      console.log(`word ${' '.repeat(longest - 4)}| translations ${' '.repeat(50 - 12)}| frequency`);
+      console.log('-'.repeat(longest + 62 + key.length))
 
-    translated.translations[key].forEach(el => {
-      const indent = longest - el.word.length;
-      console.log(`${el.word + ' '.repeat(indent)} | ${el.translations.join(', ').length < 50 ? el.translations.join(', ').padEnd(50, ' ') : el.translations.join(', ').substr(0, 47).padEnd(50, '.')} | ${el.frequency}`);
-    })
+      translated.translations[key].forEach(el => {
+        const indent = longest - el.word.length;
+        console.log(`${el.word + ' '.repeat(indent)} | ${el.translations.join(', ').length < 50 ? el.translations.join(', ').padEnd(50, ' ') : el.translations.join(', ').substr(0, 47).padEnd(50, '.')} | ${el.frequency}`);
+      })
 
-    console.log('-'.repeat(longest + 62 + key.length))
+      console.log('-'.repeat(longest + 62 + key.length))
 
+    }
   }
 
   // definitions
-  for (const key in translated.definitions) {
-    let longest = 0;
-    console.log();
+  if (translated.definitions) {
+    for (const key in translated.definitions) {
+      let longest = 0;
+      console.log();
 
-    // find longest string
-    translated.definitions[key].forEach(el => {
-      if (el.definition.length > longest) longest = el.definition.length + 1;
-    })
+      // find longest string
+      translated.definitions[key].forEach(el => {
+        if (el.definition.length > longest) longest = el.definition.length + 1;
+      })
 
-    if (longest > 50) longest = 50;
+      if (longest > 50) longest = 50;
 
-    // because it's left & right so divide by 2 and 31 is from 100 / 2 and - 1 is from spacebar from either side
-    console.log(`${'='.repeat((longest / 2) + 50 - 1)} ${key} ${'='.repeat((longest / 2) + 50 - 1)}`);
+      // because it's left & right so divide by 2 and 31 is from 100 / 2 and - 1 is from spacebar from either side
+      console.log(`${'='.repeat((longest / 2) + 50 - 1)} ${key} ${'='.repeat((longest / 2) + 50 - 1)}`);
 
-    // 100 is just brute force of the longest line to make it neat lol
-    console.log('-'.repeat(longest + 100 + key.length))
-    console.log(`definition ${' '.repeat(longest - 10)}| synonyms ${' '.repeat(50 - 8)}| example`);
-    console.log('-'.repeat(longest + 100 + key.length))
+      // 100 is just brute force of the longest line to make it neat lol
+      console.log('-'.repeat(longest + 100 + key.length))
+      console.log(`definition ${' '.repeat(longest - 10)}| synonyms ${' '.repeat(50 - 8)}| example`);
+      console.log('-'.repeat(longest + 100 + key.length))
 
-    translated.definitions[key].forEach(el => {
-      console.log(`${(el.definition.length < 50 ? el.definition.padEnd(50, ' ') : el.definition.substr(0, 47).padEnd(50, '.')) } | ${el.synonyms.join(', ').length < 50 ? el.synonyms.join(', ').padEnd(50, ' ') : el.synonyms.join(', ').substr(0, 47).padEnd(50, '.')} | ${el.example}`);
-    })
+      translated.definitions[key].forEach(el => {
+        console.log(`${(el.definition.length < 50 ? el.definition.padEnd(50, ' ') : el.definition.substr(0, 47).padEnd(50, '.'))} | ${el.synonyms.join(', ').length < 50 ? el.synonyms.join(', ').padEnd(50, ' ') : el.synonyms.join(', ').substr(0, 47).padEnd(50, '.')} | ${el.example}`);
+      })
 
-    console.log('-'.repeat(longest + 100 + key.length) + '\n')
+      console.log('-'.repeat(longest + 100 + key.length) + '\n')
 
+    }
   }
 
-  for (const key in translated.synonyms) {
-    const highlightedSyn = translated.synonyms[key].map(el => {
-      return el[0]
-    })
-    console.log(`Top Synonyms for ${key} are: ${highlightedSyn.join(', ')}\n`);
+  if (translated.synonyms) {
+    for (const key in translated.synonyms) {
+      const highlightedSyn = translated.synonyms[key].map(el => {
+        return el[0]
+      })
+      console.log(`Top Synonyms for ${key} are: ${highlightedSyn.join(', ')}\n`);
+    }
   }
 
-  console.log(`example: ${translated.examples[Math.floor(Math.random() * translated.examples.length)]}\n`);
+  if (translated.examples) console.log(`example: ${translated.examples[Math.floor(Math.random() * translated.examples.length)]}\n`);
+
   console.log(`translated from ${translated.from} to ${translated.to}`);
 
 }
 
-trans('run');
+trans('run', { to: 'id'});
